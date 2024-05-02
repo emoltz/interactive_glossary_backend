120c9eb1d3fbaf89580e089b0aa93ec2d8a9ab12
Ethan Shafran Moltz <emoltz@carnegielearning.com>
2024-05-02 16:04:23 -0400

    setup some basic api views

--- api/urls.py
+++ api/urls.py
@@ -4,10 +4,14 @@ from rest_framework_simplejwt.views import (
     TokenRefreshView,
 )
 
+from .views import GetTermsAndDefinitions, GetTerms
+
 urlpatterns = [
     # TODO figure out how to text this from angular frontend
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    # path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    # path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     # other api paths...
+    path('all_terms_by_grade_level/', GetTermsAndDefinitions.as_view(), name='all_terms_by_grade_level'),
+    path('get_terms/', GetTerms.as_view(), name='get_terms')
 
 ]
--- api/views.py
+++ api/views.py
@@ -2,3 +2,44 @@ from django.shortcuts import render
 from rest_framework import status
 from rest_framework.decorators import APIView
 from rest_framework.response import Response
+
+from .models import Terms, Definitions, ChatLog
+
+
+class GetTerms(APIView):
+    @staticmethod
+    def get(request):
+        """
+        get all terms
+        """
+        try:
+            terms = Terms.objects.all()
+            response = {
+                "terms": [term.term for term in terms]
+            }
+            return Response(response, status=status.HTTP_200_OK)
+        except Exception as e:
+            return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
+class GetTermsAndDefinitions(APIView):
+    @staticmethod
+    def post(request):
+        """
+        get all terms and definitions by grade level
+        """
+        try:
+            grade_level: str = request.data.get("grade_level")
+            terms = Terms.objects.all()
+            definitions = Definitions.objects.filter(reading_level=grade_level)
+            term_definiton_dict = {}
+            list_of_terms = [term.term for term in terms]
+            for term in list_of_terms:
+                # get definitions of each term and add to dictionary
+                term_definiton_dict[term] = [definition.definition for definition in definitions if definition.related_term.term == term]
+
+
+            response = {
+                "terms": term_definiton_dict
+            }
+            return Response(response, status=status.HTTP_200_OK)
+        except Exception as e:
+            return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
